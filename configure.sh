10ae323b19cac763d22b44b800515140b67c9ed1
elkorn <helluinster@gmail.com>
2014-01-30 20:44:29 +0100

    [walkins] Adding prompts to configuration procedure. Closes #13.

--- configure.sh
+++ configure.sh
@@ -6,26 +6,32 @@ then
     echo "Assets not found. Please make sure you are running ./configure from within walkins root directory."
 fi
 
+echo "Moving assets..."
 mkdir -p ~/.walkins/assets
-mkdir -p ~/local/bin
 chmod 755 -R ~/.walkins
 cp *.png ~/.walkins/assets/
 
+echo "Creating the configuration file..."
 touch ~/.walkins/.walkinsrc
 cat default.conf > ~/.walkins/.walkinsrc
+touch ~/.walkins/logfile
+touch ~/.walkins/.credentials
+read -p "Your Jenkins username please: " username
+echo "Your Jenkins password please: "
+read -s password
+echo "$username:$password" > ~/.walkins/.credentials
+
+read -p "The URL to the Jenkins view you want to track: " url
+sed -i.bak "s,jenkins_url_here,$url,g" ~/.walkins/.walkinsrc
+
+echo "Setting walkins up as a command..."
+mkdir -p ~/local/bin
 if [ -L ~/local/bin/walkins ]
 then
-    echo "Updating symlink to walkins..."
-
     rm ~/local/bin/walkins
 fi
 
 ln -s ./walkins.sh ~/local/bin/walkins
-
-touch ~/.walkins/logfile
-touch ~/.walkins/.credentials
-echo "username:password" > ~/.walkins/.credentials
-
 
 touch ~/.walkins/.install-path
 echo "INSTALL_PATH=$(pwd)" > ~/.walkins/.install-path
